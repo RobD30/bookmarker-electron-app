0085b72a87aa5131a2215253698f5e57831903de
Robert Dickerson <rdickeriii@gmail.com>
2017-10-08 12:48:52 -0700

    Refactor: Wrong, NOW I made a window.
    end_of_line

--- app/main.js
+++ app/main.js
@@ -2,4 +2,5 @@ const {app, BrowserWindow, } = require('electron');
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow();
+    mainWindow.loadURL('file://${__dirname}/index.html');
 });
