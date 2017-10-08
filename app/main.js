17b6960ec37bec06af5cc9f217c52b283902fccb
Robert Dickerson <rdickeriii@gmail.com>
2017-10-08 12:46:35 -0700

    Refactor: Made a window
    end_of_line

--- app/main.js
+++ app/main.js
@@ -1,19 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-
-let mainWindow = null;
+const {app, BrowserWindow, } = require('electron');
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        width: 400,
-        height: 600,
-        show: false
-    });
-
-    mainWindow.once('ready-to-show', () => {
-        main.window.show(true);
-    });
-
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+    const mainWindow = new BrowserWindow();
 });
